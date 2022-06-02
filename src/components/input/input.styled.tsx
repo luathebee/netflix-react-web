a971f532028abbca7ead63d11b361db13fcc6a72
Rubens de Barros Zanatta <rzanatta@mcfadyen.com>
2022-06-02 19:57:17 -0300

    global components and theming

---
+++ src/components/input/input.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    width: calc{100% - 18px};
+    margin 0 0 22px;
+    padding: 8px;
+    border-radius: 5px;
+    border: 1px solid #E0E0E0;
+    box-sizing: border-box;
+`;
---
+++ src/themes/main/palette.ts
@@ -0,0 +1,14 @@
+const core = {
+    primary: '#E31A13',
+    secondary: '#1a1a1a'
+}
+
+const typography = {
+    primary: '#FFFFFF',
+    contrast: '#868d96'
+}
+
+export const palette = {
+    core, 
+    typography
+}
